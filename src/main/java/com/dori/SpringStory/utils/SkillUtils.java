0f7ea681ee0c2ae2335a27f60ffed76ec45f99a8
Doriyan <doriyan13@gmail.com>
2023-04-19 00:41:15 +0300

    clean the mess i did in SkillUtils

--- src/main/java/com/dori/SpringStory/utils/SkillUtils.java
+++ src/main/java/com/dori/SpringStory/utils/SkillUtils.java
@@ -6,72 +6,46 @@ import org.springframework.stereotype.Component;
 @Component
 public interface SkillUtils {
 
-    static boolean isIgnoreMasterLevelForCommon(int nSkillID)
-    {
-        boolean retVal;
-
-        if (nSkillID > 3220010)
-        {
-            if (nSkillID <= 5220012)
-            {
-                if (nSkillID == 5220012 || nSkillID == 4120010 || nSkillID == 4220009)
-                    return true;
-                retVal = nSkillID == 5120011;
-                return retVal;
-            }
-            if (nSkillID != 32120009)
-            {
-                retVal = nSkillID == 33120010;
-                return retVal;
-            }
-            return true;
-        }
-        if (nSkillID >= 3220009)
-            return true;
-        if (nSkillID > 2120009)
-        {
-            if (nSkillID > 2320010)
-            {
-                return nSkillID >= 3120010 && nSkillID <= 3120011;
-            }
-            else if (nSkillID != 2320010)
-            {
-                retVal = nSkillID == 2220009;
-                return retVal;
-            }
-            return true;
-        }
-        if (nSkillID == 2120009 || nSkillID == 1120012 || nSkillID == 1220013)
-            return true;
-
-        return nSkillID == 1320011;
+    private static boolean isIgnoreMasterLevelForCommon(int nSkillID) {
+        return nSkillID == 3220009
+                || nSkillID == 3220010
+                || nSkillID == 4120010
+                || nSkillID == 4220009
+                || nSkillID == 5220012
+                || nSkillID == 2120009
+                || nSkillID == 1120012
+                || nSkillID == 1220013
+                || nSkillID == 1320011
+                || nSkillID == 2220009
+                || nSkillID == 2320010
+                || nSkillID == 3120010
+                || nSkillID == 3120011
+                || nSkillID == 32120009
+                || nSkillID == 33120010
+                ;
     }
 
     static boolean isSkillNeedMasterLevel(int skillId) {
-        // TODO: need to refactor this all part (currently it's raw ida stuff!)
-        if(isIgnoreMasterLevelForCommon(skillId)){
+        if (isIgnoreMasterLevelForCommon(skillId)) {
             return false;
         }
         int nJob = skillId / 10_000;
-        if(skillId / 10_000 / 100 == 22 || nJob == Job.Evan.getId()){
+
+        if (nJob / 100 == 22 || nJob == Job.Evan.getId()) {
             int jobLvl = JobUtils.getJobLevel(nJob);
-            return jobLvl == 9
-                    || jobLvl == 10
+            return jobLvl == 9 || jobLvl == 10
                     || skillId == 22111001
                     || skillId == 22141002
-                    || skillId == 22140000 ;
+                    || skillId == 22140000;
         }
-        if(nJob / 10 == 43){
+        if (nJob / 10 == 43) {
             return JobUtils.getJobLevel(nJob) == 4
-                    || skillId == 4311003
+                    || skillId == 4130004
                     || skillId == 4321000
-                    || skillId == 4331002
-                    || skillId == 4331005 ;
+                    || skillId == 4331004
+                    || skillId == 4331006;
         }
-        if(nJob == 100 * skillId / 10_000 / 100){
-            return false;
-        }
-        return nJob % 10 == 2;
+        return false;
     }
 
 
